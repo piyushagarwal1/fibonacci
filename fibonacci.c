af2892d7ee45acb320ed9f9bdc6a000cfaa56b05
piyushagarwal1 <37872135+piyushagarwal1@users.noreply.github.com>
2018-03-28 21:10:56 +0530

    Update fibonacci.c
    fibonacci series using dp

--- fibonacci.c
+++ fibonacci.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
-int main()
-print f
-return
+long int fibonacci(int n){
+  int f[n+1];
+  f[0]=0;
+  f[1]=1;
+  int i;
+  for(i=2;i<=n;i++){
+    f[i]=f[i-1]+f[i-2];
+  }
+  return f[n];
+}
+int main(){
+  int n;
+  long int ans;
+  printf("Enter the number");
+  scanf("%d",&n);
+  ans=fibonacci(n);
+  printf("The %dth fibonacci number is %d",n,ans);
+  return 0;
+}
+  
